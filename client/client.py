74cee7416162e85da7828b5ce15d2985d317cd62
STephanDolskii <STephanDolskii@ya.ru>
2023-05-19 19:23:13 +0300

    finally

--- client/client.py
+++ client/client.py
@@ -6,6 +6,6 @@ api_id = id
 api_hash = hash
 
 # starting telethon client
-client = telethon.TelegramClient('sergay', api_id, api_hash, system_version="4.16.30-vxCUSTOM")
+client = telethon.TelegramClient('your_client', api_id, api_hash, system_version="4.16.30-vxCUSTOM")
 client.start()
 
--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 # telethon
-id = 28552410
-hash = '1542343743d2799f779bae0b3071a67c'
-main_channel = -1001957389851  # tttestchenneel
 
-bot_chat = "https://t.me/AutoCommentCommandBot"
+id = 000 # напишите ваш id в поле без кавычек
+hash = '<вот тут напишите ваш hash>'  # напишите сюда ваш hash В КАВЫЧКАХ (любых)
+main_channel = '@tttestchanneel'
 
-token_api = '6009272293:AAGDFtphyBoAuSqjMSF6bnQ9Oit8k3wlEe0'
